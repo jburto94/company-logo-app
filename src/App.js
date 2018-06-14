d08b5a56d5d110ee010802c79917baf273e1f7bc
jburto94 <jburto94@yahoo.com>
2018-06-13 21:27:17 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      sizeTerm: '250',
+      searchImage: 'google.com/',
+      imageSize: '250'
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onSizeChange = this.onSizeChange.bind(this);
+  }
+
+
+  onSearchChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  onSizeChange(e) {
+    this.setState({ sizeTerm: e.target.value });
+  }
+
+  onSearchSubmit(e) {
+    e.preventDefault();
+    const {searchTerm, sizeTerm} = this.state;
+    this.setState({ 
+      searchImage: searchTerm,
+      imageSize: sizeTerm
+    });
+  }
+
+  render() {
+    const URL = 'https://logo.clearbit.com/';
+    const {searchImage, imageSize} = this.state;
+
+    return (
+      <div className="App">
+        <form onSubmit={this.onSearchSubmit}>
+          <input type='text'
+                 placeholder='Enter Company URL Here'
+                 onChange={this.onSearchChange} />
+          <input type='text'
+                 placeholder='Enter Company URL Here'
+                 onChange={this.onSizeChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <img src={`${URL}${searchImage}?size=${imageSize}`} 
+             alt={searchImage} />
+      </div>
+    );
+  }
+}
+
+export default App;
